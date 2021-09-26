b0ab39f88b9067942cf1f3da9dfb2574a5890a45
Lesly <leslyr562@gmail.com>
2021-09-26 16:17:03 -0500

    formatted javascript file

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,27 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-var today = moment().format("dddd MMM D YYYY");
-var dateEl = document.getElementById('currentDay');
-console.log("today", today);
-dateEl.innerHTML = today;
-var currentHour = moment().format("HH");
+    var today = moment().format("dddd MMM D YYYY");
+    var dateEl = document.getElementById('currentDay');
+    console.log("today", today);
+    dateEl.innerHTML = today;
+    var currentHour = moment().format("HH");
 
-$(".time-div").each(function(){
-    var hour = $(this).attr("id").split("-")[1];
+    $(".time-div").each(function () {
+        var hour = $(this).attr("id").split("-")[1];
 
-    if (currentHour === hour) {
-        $(this).addClass("present"); {
-            $(this).addClass("present");
-            $(this).children(".description").addClass("white-text");
+        if (currentHour === hour) {
+            $(this).addClass("present"); {
+                $(this).addClass("present");
+                $(this).children(".description").addClass("white-text");
+            }
+        } else if (currentHour < hour) {
+            $(this).removeClass("present");
+            $(this).addClass("future");
+        } else if (currentHour > hour) {
+            $(this).removeClass("future");
+            $(this).addClass("past");
         }
-    } else if (currentHour < hour) {
-        $(this).removeClass("present");
-        $(this).addClass("future");
-    } else if (currentHour > hour){
-        $(this).removeClass("future");
-        $(this).addClass("past");
-    }
-})
+    })
 });
 
 
